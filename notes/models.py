633d188a7b27ceda139ff3949124752f37428cfb
Nikhil Kamineni <nikhil.kamineni@gmail.com>
2018-06-25 13:54:09 -0400

    Fix notes app path

---
+++ notes/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from uuid import uuid4
+
+class Note(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    # TODO: Add user/author who created it
+    title = models.CharField(max_length=200)
+    content = models.TextField('Note', blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
+    # TODO: Tagging system or categories
